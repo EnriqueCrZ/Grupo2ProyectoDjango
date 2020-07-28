4fb63b888ef54c74cbf29efbb8039e5205431a87
AlphaRev <enriquecrz21@gmail.com>
2020-07-28 13:39:41 -0600

    EmailField added

--- Models/Sucursal/models.py
+++ Models/Sucursal/models.py
@@ -6,5 +6,5 @@ class Sucursal(models.Model):
     nombre = models.CharField(max_length=45)
     razon_social = models.CharField(max_length=45)
     direccion = models.CharField(max_length=125)
-    correo = models.CharField(max_length=125)
+    correo = models.EmailField(max_length=125)
     telefono = models.CharField(max_length=10)
--- Models/Usuario/models.py
+++ Models/Usuario/models.py
@@ -9,6 +9,6 @@ class TipoUsuario(models.Model):
 class Usuario(models.Model):
     id_user = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=45)
-    correo = models.CharField(max_length=125)
+    correo = models.EmailField(max_length=125)
     password = models.CharField(max_length=45)
     tipo_usuario_idtipo_usuario = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
